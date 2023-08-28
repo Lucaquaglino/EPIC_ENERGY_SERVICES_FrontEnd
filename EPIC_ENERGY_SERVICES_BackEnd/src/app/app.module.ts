31d31e1e0d26a4d4cb28fbde1557c26851e73192
Luca Quaglino <quagli.luca@gmail.com>
2023-08-28 19:30:21 +0200

    set up component routing

--- EPIC_ENERGY_SERVICES_BackEnd/src/app/app.module.ts
+++ EPIC_ENERGY_SERVICES_BackEnd/src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ClientiComponent } from './components/clienti/clienti.component';
+import { FattureComponent } from './components/fatture/fatture.component';
+const rotte: Route[] = [
+  // { path: '', redirectTo: 'register', pathMatch: 'full' },
+  {
+      path: 'clienti',
+      component: ClientiComponent,
+      // canActivate: [AuthGuard]
+  },
+  {
+      path: 'fatture',
+      component: FattureComponent,
+      // canActivate: [AuthGuard]
+   }
+// ,
+//   {
+//     path: 'infoFIlm/:id',
+//     component: InfoFilmComponent,
+//     // canActivate: [AuthGuard]
+// },
+//   {
+//       path: 'login',
+//       component: LoginComponent
+//   },
+//   {
+//       path: 'register',
+//       component: RegisterComponent
+//   },
+//   {
+//     path: 'navBar',
+//     component: NavBarComponent,
+//     canActivate: [AuthGuard]
+// },
+// {
+//   path: 'profilo',
+//   component: ProfileComponent,
+//   canActivate: [AuthGuard]
+// },
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientiComponent,
+    FattureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rotte)
   ],
   providers: [],
   bootstrap: [AppComponent]
