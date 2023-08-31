4fac8b7a5981298fdcd6edcfd5347a4acf6d7447
Luca Quaglino <quagli.luca@gmail.com>
2023-08-31 11:23:07 +0200

    Update app.service.ts

--- EPIC_ENERGY_SERVICES_BackEnd/src/app/services/app.service.ts
+++ EPIC_ENERGY_SERVICES_BackEnd/src/app/services/app.service.ts
@@ -4,12 +4,13 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Clienti } from '../models/clienti.interface';
 import { map } from 'rxjs';
+import { Fattura } from '../models/fattura.interface';
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
 
-  private baseUrl = 'http://localhost:3001/clienti'; // Controlla l'URL del backend
+  private urlClienti = 'http://localhost:3001/clienti'; // Controlla l'URL del backend
 
   constructor(private http: HttpClient) { }
 
@@ -19,19 +20,19 @@ export class AppService {
       .set('order', order);
 
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem('token')}`
+      Authorization: Bearer ${localStorage.getItem('token')}
     });
 
-    return this.http.get<any>(this.baseUrl, { params, headers })
+    return this.http.get<any>(this.urlClienti, { params, headers })
       .pipe(map(response => response.content));
   }
 
 
 
   getProvinciaById(provincia: string): Observable<Clienti> {
-    const url = `${this.baseUrl}/${provincia}`;
+    const url = ${this.urlClienti}/${provincia};
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem('token')}`
+      Authorization: Bearer ${localStorage.getItem('token')}
     });
     return this.http.get<Clienti>(url, { headers });
   }
@@ -40,24 +41,54 @@ export class AppService {
 
 creaCliente(cliente: Clienti): Observable<Clienti> {
   const headers = new HttpHeaders({
-    Authorization: `Bearer ${localStorage.getItem('token')}`
+    Authorization: Bearer ${localStorage.getItem('token')}
   });
-  return this.http.post<Clienti>(this.baseUrl, cliente, { headers });
-}
+  return this.http.post<Clienti>(this.urlClienti, cliente, { headers });
 }
 
+// ------------------------------------------------------ Fattura
+
+private urlFattura = 'http://localhost:3001/fattura';
+
+creaFattura(fattura: Fattura): Observable<Fattura>{
+  const headers = new HttpHeaders({
+    Authorization: Bearer ${localStorage.getItem('token')}
+  });
+  return this.http.post<Fattura>(this.urlFattura, fattura, { headers });
+ }
+
+ getFatture(page: number, order: string): Observable<Fattura[]> {
+   const params = new HttpParams()
+   .set('page', page.toString())
+   .set('order', order);
+
+   const headers = new HttpHeaders({
+     Authorization: Bearer ${localStorage.getItem('token')}
+    });
+
+    return this.http.get<any>(this.urlFattura, { params, headers })
+    .pipe(map(response => response.content));
+  }
+
+  getFatturaById(fattura: string): Observable<Fattura> {
+    const url = ${this.urlFattura}/${fattura};
+    const headers = new HttpHeaders({
+      Authorization: Bearer ${localStorage.getItem('token')}
+    });
+    return this.http.get<Fattura>(url, { headers });
+  }
+
+}
 //   //CHIAMATA POST PER METTERE FILM NEI PREFERITI
 //   aggiungiFavorites(data: Favorites) {
 //     return this.http.post<Favorites>('http://localhost:4201/favorites', data);
 //   }
 //   // CHIAMATA DELETE PER ELEMINARE FILM DAI PREFERITI
 //   eliminaFavorites(favoriteID: number) {
-//     return this.http.delete(`http://localhost:4201/favorites/${favoriteID}`);
+//     return this.http.delete(http://localhost:4201/favorites/${favoriteID});
 //   }
 // //CHIAMATA GET PER INFO FILM SPECIFICO
 //   getFilmById(id: number) {
-//     return this.http.get<Movies[]>(`http://localhost:4201/movies-popular/${id}`);
+//     return this.http.get<Movies[]>(http://localhost:4201/movies-popular/${id});
 //   }
-
-
 
